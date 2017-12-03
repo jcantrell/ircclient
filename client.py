510b9d06a343589f4260b55780480881fcfc763e
Jordan Cantrell <cantrel2@pd.edu>
2017-12-02 17:10:53 -0800

    Added barebones examples

---
+++ client.py
@@ -0,0 +1,20 @@
+import socket
+import sys
+
+if len(sys.argv) < 4:
+    print "USAGE: echo_client_sockets.py <HOST> <PORT> <MESSAGE>";
+    sys.exit(0)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = sys.argv[1]
+port = int(sys.argv[2])
+s.connect((host,port))
+
+s.send(sys.argv[3])
+
+i = 0
+data = s.recv(10000000)
+print data
+print 'received', len(data), ' bytes'
+s.close()
